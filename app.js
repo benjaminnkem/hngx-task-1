5d5cc70ebd075a5d598efaed0e5f03f1f2eb36fa
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-10 01:48:52 +0100

    rounded up task 1 for now

---
+++ app.js
@@ -0,0 +1,13 @@
+const dayOfTheWeekEl = document.querySelector("#dayOfTheWeek");
+const currentUTCTimeEl = document.querySelector("#currentUTCTime");
+
+const daysInAWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const updateValues = () => {
+  dayOfTheWeekEl.textContent = daysInAWeek[new Date().getDay()];
+  currentUTCTimeEl.textContent = new Date().getTime();
+};
+
+updateValues(); // for initial updating
+
+setInterval(() => updateValues(), 1000); // updates every seconds
